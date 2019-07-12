e1f978145e77bb6aa6c8ffe186ccb39663d8b886
Vandan Patel <tech.h4x0rz@gmail.com>
2019-07-12 15:06:50 -0400

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,28 @@
+ATL_Pragna_Scores =	{
+  "Shivam Patel": 30,
+  "Tejas Shah": 55,
+  "Yogesh Modi": 68,
+  "Jayesh Kumar": 14,
+  "Ghanshyam Trivedi": 51,
+  "Umesh Bhatt": 23,
+  "Ieshesh Patel": 10,
+  "Hitesh Patel": 71,
+  "Parth Soni": 91,
+  "Vandan Patel": 100,
+  "Ramesh Gandhi": 63,
+  "Sanjay Parikh": 86
+}
+
+#Get scores array
+https://www.w3schools.com/python/python_dictionaries.asp
+
+#two types of for loops (foreach and using range(0,len))
+https://www.w3schools.com/python/python_dictionaries.asp
+
+#Find Avg, Mean, Median Scores
+
+#Find Min/Max -> now return name (two methods, dictionary or for loop then indexing)
+
+#Sort array (sorting algorithms discoveries)
+
+
